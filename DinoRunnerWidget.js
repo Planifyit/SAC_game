1f694620e6de725200ab4cc3b68d9bba4d802a9e
planifyit.com <contact@planifyit.com>
2023-05-21 18:00:43 +0000

    Update DinoRunnerWidget.js

--- DinoRunnerWidget.js
+++ DinoRunnerWidget.js
@@ -23,14 +23,12 @@
                 height: 50px;
                 width: 50px;
                 background: red;
+                animation: moveObstacle 2s linear infinite;
             }
             .score {
                 position: absolute;
                 right: 10px;
                 top: 10px;
-            }
-            .animateObstacle {
-                animation: moveObstacle 2s linear infinite;
             }
             @keyframes moveObstacle {
                 0% { right: 0; }
@@ -43,6 +41,8 @@
         </div>
         <button class="start-button">Start Game</button>
         <button class="replay-button" style="display: none;">Play Again</button>
+        <button class="jump-button" style="display: none;">Jump</button>
+        <button class="pause-button" style="display: none;">Pause</button>
     `;
 
     class DinoRunner extends HTMLElement {
@@ -54,14 +54,19 @@
             this._gameContainer = this._shadowRoot.querySelector('.game-container');
             this._startButton = this._shadowRoot.querySelector('.start-button');
             this._replayButton = this._shadowRoot.querySelector('.replay-button');
+            this._jumpButton = this._shadowRoot.querySelector('.jump-button');
+            this._pauseButton = this._shadowRoot.querySelector('.pause-button');
             this._scoreDisplay = this._shadowRoot.querySelector('.score');
 
             this._score = 0;
+            this._isPaused = false;
         }
 
         connectedCallback() {
             this._startButton.addEventListener('click', this._startGame.bind(this));
             this._replayButton.addEventListener('click', this._replayGame.bind(this));
+            this._jumpButton.addEventListener('click', this._jump.bind(this));
+            this._pauseButton.addEventListener('click', this._pause.bind(this));
         }
 
         _startGame() {
@@ -69,12 +74,15 @@
             this._obstacle = document.createElement('div');
             this._obstacle.classList.add('obstacle');
             this._gameContainer.appendChild(this._obstacle);
-            this._obstacle.classList.add('animateObstacle');
             this._gameInterval = setInterval(this._gameLoop.bind(this), 50);
             this._startButton.style.display = 'none';
+            this._jumpButton.style.display = 'block';
+            this._pauseButton.style.display = 'block';
         }
 
         _gameLoop() {
+            if (this._isPaused) return;
+
             const playerRect = this._player.getBoundingClientRect();
             const obstacleRect = this._obstacle.getBoundingClientRect();
 
@@ -94,7 +102,6 @@
                 this._obstacle = document.createElement('div');
                 this._obstacle.classList.add('obstacle');
                 this._gameContainer.appendChild(this._obstacle);
-                this._obstacle.classList.add('animateObstacle');
             }
         }
 
@@ -103,6 +110,8 @@
             this._obstacle.remove();
             alert('Game Over!');
             this._replayButton.style.display = 'block';
+            this._jumpButton.style.display = 'none';
+            this._pauseButton.style.display = 'none';
         }
 
         _replayGame() {
@@ -110,6 +119,19 @@
             this._scoreDisplay.textContent = 'Score: ' + this._score;
             this._replayButton.style.display = 'none';
             this._startGame();
+        }
+
+        _jump() {
+            let initialBottom = this._player.style.bottom;
+            this._player.style.bottom = '100px';
+            setTimeout(() => {
+                this._player.style.bottom = initialBottom;
+            }, 500);
+        }
+
+        _pause() {
+            this._isPaused = !this._isPaused;
+            this._pauseButton.textContent = this._isPaused ? 'Resume' : 'Pause';
         }
     }
 
