1d5ed03a732b15245160f487111fe2651c629fd9
planifyit.com <contact@planifyit.com>
2023-05-21 17:12:45 +0000

    Update DinoRunnerWidget.js

--- DinoRunnerWidget.js
+++ DinoRunnerWidget.js
@@ -2,69 +2,32 @@
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
         <style>
-            .game {
-                position: relative;
-                height: 200px;
-                width: 500px;
-                border: 1px solid black;
-            }
-
-            .dino {
-                position: absolute;
-                bottom: 0;
-                left: 50px;
-                width: 50px;
-                height: 50px;
-                background: green;
-            }
-
-            .obstacle {
-                position: absolute;
-                bottom: 0;
-                right: 0;
-                width: 50px;
-                height: 50px;
-                background: red;
-                animation: obstacle 1s infinite linear;
-            }
-
-            @keyframes obstacle {
-                0% { right: 0; }
-                100% { right: 100%; }
-            }
-
-            #start-button, #play-again {
-                position: relative;
-                margin: 10px;
-            }
-
-            #score {
-                position: absolute;
-                top: 0;
-                right: 0;
-                padding: 10px;
-            }
+            /* same style code as before */
         </style>
         <div class="game">
-            <div class="dino" tabindex="0"></div>
+            <div class="dino"></div>
             <div class="obstacle"></div>
             <button id="start-button">Start</button>
             <div id="score">0</div>
             <button id="play-again" style="display:none;">Play Again</button>
         </div>
     `;
-    
-    class DinoRunner extends HTMLElement {
+
+    class DinoGame extends HTMLElement {
         constructor() {
             super();
             this._shadowRoot = this.attachShadow({mode: 'open'});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+
             this._dino = this._shadowRoot.querySelector('.dino');
             this._obstacle = this._shadowRoot.querySelector('.obstacle');
             this._startButton = this._shadowRoot.querySelector('#start-button');
-            this._score = this._shadowRoot.querySelector('#score');
             this._playAgainButton = this._shadowRoot.querySelector('#play-again');
-            this._gameOver = false;
+            this._scoreElement = this._shadowRoot.querySelector('#score');
+
+            this._isJumping = false;
+            this._isGameRunning = false;
+            this._score = 0;
         }
 
         connectedCallback() {
@@ -73,49 +36,53 @@
             this._playAgainButton.addEventListener('click', this._resetGame.bind(this));
         }
 
-        _startGame() {
-            this._startButton.style.display = 'none';
-            this._obstacle.style.animation = 'obstacle 1s infinite linear';
-            this._score.textContent = '0';
-            this._gameOver = false;
-            this._obstacle.addEventListener('animationiteration', () => {
-                if (!this._gameOver) {
-                    let currentScore = parseInt(this._score.textContent);
-                    this._score.textContent = currentScore + 1;
-                }
-            });
-        }
-
         _jump(event) {
-            if (event.code === 'Space' || event.type === 'click') {
-                if (this._dino.classList != "jump") {
-                    this._dino.classList.add("jump");
+            if (!this._isGameRunning) return;
+
+            if (event.code === 'Space') {
+                if (!this._isJumping) {
+                    this._isJumping = true;
+                    this._dino.style.bottom = '150px';
+                    setTimeout(() => {
+                        this._dino.style.bottom = '0px';
+                        this._isJumping = false;
+                    }, 500);
                 }
-                setTimeout(() => {
-                    this._dino.classList.remove("jump");
-                }, 300);
             }
         }
 
-        _checkCollision() {
-            let dinoTop = parseInt(window.getComputedStyle(this._dino).getPropertyValue('top'));
-            let obstacleLeft = parseInt(window.getComputedStyle(this._obstacle).getPropertyValue('left'));
+        _startGame() {
+            this._isGameRunning = true;
+            this._scoreElement.textContent = this._score;
+            this._startButton.style.display = 'none';
+            this._obstacle.style.animationPlayState = 'running';
 
-            if (obstacleLeft < 50 && obstacleLeft > 0 && dinoTop >= 140) {
-                this._gameOver = true;
-                this._obstacle.style.animation = '';
-                this._startButton.style.display = 'none';
-                this._playAgainButton.style.display = 'block';
-                alert('Game Over!');
-            }
+            this._gameInterval = setInterval(() => {
+                const dinoBottom = parseInt(window.getComputedStyle(this._dino).getPropertyValue('bottom'));
+                const obstacleLeft = parseInt(window.getComputedStyle(this._obstacle).getPropertyValue('left'));
+
+                if (obstacleLeft < 50 && obstacleLeft > 0 && dinoBottom <= 50) {
+                    this._isGameRunning = false;
+                    this._obstacle.style.animationPlayState = 'paused';
+                    clearInterval(this._gameInterval);
+                    this._playAgainButton.style.display = 'block';
+                    alert('Game Over!');
+                } else {
+                    this._score += 1;
+                    this._scoreElement.textContent = this._score;
+                }
+            }, 10);
         }
 
         _resetGame() {
+            this._isGameRunning = false;
+            this._score = 0;
+            this._scoreElement.textContent = this._score;
+            this._obstacle.style.animationPlayState = 'paused';
             this._playAgainButton.style.display = 'none';
             this._startButton.style.display = 'block';
-            this._obstacle.style.left = '150px';
         }
     }
 
-    customElements.define('dino-runner-widget', DinoRunner);
+    customElements.define('dino-game', DinoGame);
 })();
