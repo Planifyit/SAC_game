031d01503ac7e8c33f04b73f702e4a7af0399543
planifyit.com <contact@planifyit.com>
2023-05-24 08:37:08 +0200

    Update DinoRunnerWidget.js

--- DinoRunnerWidget.js
+++ DinoRunnerWidget.js
@@ -2,7 +2,12 @@
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
         <style>
-        
+          .game-container {
+                position: relative;
+                height: 200px;
+                width: 300px;
+                border: 1px solid black;
+            }
           
             .player, .obstacle {
                 position: absolute;
